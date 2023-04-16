ba02285b3b94f013b35d4dfb08b941cd7507e9d8
DESKTOP-EAGQPHJ\Angus Wang <wang2673@wisc.edu>
2023-04-16 09:37:06 -0500

    Built project, wrote merge sort

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Angus Wang/Desktop/project-3
+# Build directory: C:/Users/Angus Wang/Desktop/project-3/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_file "test_file")
+set_tests_properties(test_file PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Angus Wang/Desktop/project-3/CMakeLists.txt;19;add_test;C:/Users/Angus Wang/Desktop/project-3/CMakeLists.txt;0;")
+add_test(test_join "test_join")
+set_tests_properties(test_join PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Angus Wang/Desktop/project-3/CMakeLists.txt;24;add_test;C:/Users/Angus Wang/Desktop/project-3/CMakeLists.txt;0;")
--- src/join.cpp
+++ src/join.cpp
@@ -7,10 +7,63 @@
 JoinAlgorithm getJoinAlgorithm() {
   // TODO: Return your chosen algorithm.
   // return JOIN_ALGORITHM_BNLJ;
-  // return JOIN_ALGORITHM_SMJ;
+  return JOIN_ALGORITHM_SMJ;
   // return JOIN_ALGORITHM_HJ;
   throw std::runtime_error("not implemented: getJoinAlgorithm");
 };
+
+std::vector<Tuple> merge(std::vector<Tuple> unsorted1, std::vector<Tuple> unsorted2) {
+  std::vector<Tuple> sortedTuples;
+  int i, j;
+  i = 0;
+  j = 0;
+  
+  while (i < unsorted1.size() && j < unsorted2.size()) {
+      if (unsorted1.at(i).first > unsorted2.at(j).first) {
+          sortedTuples.emplace_back(unsorted2.at(j));
+          j++;
+      }
+      else {
+          sortedTuples.emplace_back(unsorted1.at(i));
+          i++;
+      }
+  }
+  if (i == unsorted1.size()) {
+      while (j < unsorted2.size()) {
+          sortedTuples.emplace_back(unsorted2.at(j));
+          j++;
+      }
+  }
+  else if (j == unsorted2.size()) {
+      while (i < unsorted1.size()) {
+          sortedTuples.emplace_back(unsorted1.at(i));
+          i++;
+      }
+  }
+  return sortedTuples;
+}
+
+std::vector<Tuple> mergeSort(std::vector<Tuple> unsortedTuples) {
+    if (unsortedTuples.size() == 1) {
+        return unsortedTuples;
+    }
+    std::vector<Tuple> unsorted1(unsortedTuples.size() / 2);
+    std::vector<Tuple> unsorted2(unsortedTuples.size() / 2);
+
+    for (int i = 0; i < unsortedTuples.size(); i++) {
+        if (i < unsorted1.size()) {
+            unsorted1.at(i) = unsortedTuples.at(i);
+        }
+        else {
+            unsorted2.at(i - unsortedTuples.size() / 2) = unsortedTuples.at(i);
+        }
+    }
+    unsorted1 = mergeSort(unsorted1);
+    unsorted2 = mergeSort(unsorted2);
+
+    return merge(unsorted1, unsorted2);
+
+}
 
 int join(File &file, int numPagesR, int numPagesS, char *buffer,
          int numFrames) {
@@ -21,7 +74,6 @@ int join(File &file, int numPagesR, int numPagesS, char *buffer,
   // constraints on peak heap memory usage for large input tables. You should
   // delete this code and replace it with your disk-based join algorithm
   // implementation.
-
   int pageIndexR = 0;
   int pageIndexS = pageIndexR + numPagesR;
   int pageIndexOut = pageIndexS + numPagesS;
@@ -31,17 +83,44 @@ int join(File &file, int numPagesR, int numPagesS, char *buffer,
 
   std::vector<Tuple> tuplesS(numPagesS * 512);
   file.read(tuplesS.data(), pageIndexS, numPagesS);
-
+  tuplesR = mergeSort(tuplesR);
+  tuplesS = mergeSort(tuplesS);
   std::vector<Tuple> tuplesOut;
 
-  for (const Tuple &tupleR : tuplesR) {
-    for (const Tuple &tupleS : tuplesS) {
-      if (tupleR.first == tupleS.first) {
-        tuplesOut.emplace_back(tupleR.second, tupleS.second);
-        break;
-      }
+  Tuple &tupleR = tuplesR.at(0);
+  Tuple &tupleS = tuplesS.at(0);
+  int indexR, indexS;
+  indexR = 0;
+  indexS = 0;
+  while (tupleR != tuplesR.at(tuplesR.size() - 1) && tupleS != tuplesS.at(tuplesS.size() - 1)) {
+    while (tupleR.first < tupleS.first) {
+      indexR++;
+      tupleR = tuplesR.at(indexR);
     }
+    while (tupleR.first > tupleS.first) {
+      indexS++;
+      tupleS = tuplesS.at(indexS);
+    }
+    Tuple &tupleT = tupleS;
+    int indexT = indexS;
+    while (tupleR.first == tupleS.first) {
+      tuplesOut.emplace_back(tupleR.second, tupleS.second);
+      indexS++;
+      tupleS = tuplesS.at(indexS);
+    }
+    tupleS = tupleT;
+    indexR++;
+    indexS = indexT;
+    tupleR = tuplesR.at(indexR);
   }
+  // for (const Tuple &tupleR : tuplesR) {
+  //   for (const Tuple &tupleS : tuplesS) {
+  //     if (tupleR.first == tupleS.first) {
+  //       tuplesOut.emplace_back(tupleR.second, tupleS.second);
+  //       break;
+  //     }
+  //   }
+  // }
 
   int numTuplesOut = (int)tuplesOut.size();
   int numPagesOut = numTuplesOut / 512 + (numTuplesOut % 512 != 0);
